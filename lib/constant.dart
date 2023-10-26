871b480978e7b06332b854bd60521cc8526dd60f
RavenVonGray <raven.gray@conexus-tech.com>
2023-10-26 23:29:34 +0800

    fix: change location

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,8 +1,8 @@
 class PackageConstant {
   static const barcodeFilePath =
       "packages/simple_barcode_scanner/assets/barcode.html";
-  static const priceTagFilePath =
-      "packages/simple_barcode_scanner/assets/pricetag.html";
+  static const priceTagFileWebPath =
+      "assets/packages/simple_barcode_scanner/assets/pricetag.html";
   static const barcodeFileWebPath =
       "assets/packages/simple_barcode_scanner/assets/barcode.html";
 }
